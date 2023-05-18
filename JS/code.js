4a764c900ce2394e8bef1732c14436f50fda7854
Carlos Eduardo Pereira Almeida <105117942+carlospalmeida@users.noreply.github.com>
2023-05-18 16:59:09 -0300

    18/05/2023

--- JS/code.js
+++ JS/code.js
@@ -1,12 +1,3 @@
-var fecharpopup = document.getElementById("fecharuse");
 
-fecharpopup.addEventListener("click", ()=>{
-    var popup = document.getElementById("usecarona");
-    popup.style.display = "none";
-    // if(popup.style.display === "block"){
-    //      popup.style.display = "block";
-    //  }else{
-    //      popup.style.display = "none";
-    // }
 
- })
+ 
